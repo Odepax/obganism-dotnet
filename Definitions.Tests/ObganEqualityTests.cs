6d4976e1f649eb04aca47a8792364e9fb991cad8
Odepax <odepaxforuma@gmail.com>
2019-07-30 18:22:33 +0200

    Added obgan definition

---
+++ Definitions.Tests/ObganEqualityTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+namespace Obganism.Definitions.Tests
+{
+	public class ObganEqualityTests
+	{
+		[Test]
+		[TestCaseSource(nameof(EqualSamples))]
+		public void These_obgans_are_equal(object left, object right)
+		{
+			Assert.IsTrue(left.Equals(right));
+			Assert.AreEqual(left.GetHashCode(), right.GetHashCode());
+		}
+
+		public static readonly object[] EqualSamples =
+		{
+			new[]
+			{
+				new Obgan(new Type("t")),
+				new Obgan(new Type("t"))
+			},
+			new[]
+			{
+				new Obgan(new Type("t", new Type("a"))),
+				new Obgan(new Type("t", new Type("a")))
+			},
+			new[]
+			{
+				new Obgan(new Type("t"), new Property("a", new Type("a"))),
+				new Obgan(new Type("t"), new Property("a", new Type("a")))
+			},
+			new[]
+			{
+				new Obgan(new Type("t"), new Property("a", new Type("a")), new Property("b", new Type("b"))),
+				new Obgan(new Type("t"), new Property("a", new Type("a")), new Property("b", new Type("b")))
+			}
+		};
+
+		[Test]
+		[TestCaseSource(nameof(DifferentSamples))]
+		public void These_obgans_are_not_equal(object left, object right)
+		{
+			Assert.IsFalse(left.Equals(right));
+			Assert.AreNotEqual(left.GetHashCode(), right.GetHashCode());
+		}
+
+		public static readonly object[] DifferentSamples =
+		{
+			new[]
+			{
+				new Obgan(new Type("t")),
+				new Obgan(new Type("u"))
+			},
+			new[]
+			{
+				new Obgan(new Type("t", new Type("a"))),
+				new Obgan(new Type("t", new Type("b")))
+			},
+			new[]
+			{
+				new Obgan(new Type("t"), new Property("a", new Type("a"))),
+				new Obgan(new Type("t"), new Property("b", new Type("a")))
+			},
+			new[]
+			{
+				new Obgan(new Type("t"), new Property("a", new Type("a"))),
+				new Obgan(new Type("t"), new Property("a", new Type("b")))
+			},
+			new[]
+			{
+				new Obgan(new Type("t"), new Property("a", new Type("a")), new Property("b", new Type("b"))),
+				new Obgan(new Type("t"), new Property("b", new Type("b")), new Property("a", new Type("a")))
+			}
+		};
+	}
+}
---
+++ Definitions/Obgan.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Obganism.Definitions
+{
+	public sealed class Obgan : IEquatable<Obgan>
+	{
+		public Type Type { get; }
+		public IReadOnlyList<Property> Properties { get; }
+
+		public Obgan(Type type, IEnumerable<Property> properties)
+		{
+			Type = type;
+			Properties = new List<Property>(properties);
+		}
+
+		public Obgan(Type type, params Property[] properties) : this(type, properties as IEnumerable<Property>)
+		{
+		}
+
+		public override bool Equals(object? obj) =>
+			!(obj is null) && (
+				   ReferenceEquals(this, obj)
+				|| obj is Obgan other && Equals(other)
+			);
+
+		public bool Equals(Obgan other) =>
+			ReferenceEquals(this, other) || (
+				   Type.Equals(other.Type)
+				&& Properties.SequenceEqual(other.Properties)
+			);
+
+		public override int GetHashCode()
+		{
+			HashCode hashCode = new HashCode();
+
+			hashCode.Add(Type);
+
+			foreach (Property property in Properties)
+			{
+				hashCode.Add(property);
+			}
+
+			return hashCode.ToHashCode();
+		}
+
+		/// <summary>
+		/// This method is intended for debug purposes.
+		/// It will not take part in API versionning.
+		/// </summary>
+		public override string ToString() =>
+			$"{ Type }{ (Properties.Count == 0 ? "" : $" {{ { string.Join(", ", Properties) } }}") }";
+	}
+}
