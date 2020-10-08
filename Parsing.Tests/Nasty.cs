495996a5f458723c886e93856be33205686f3d79
Odepax <odepaxforuma@gmail.com>
2020-10-08 12:18:30 +0100

    Added failing test

---
+++ Parsing.Tests/Nasty.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Obganism.Parsing.Tests
+{
+	class Nasty
+	{
+		[Test]
+		public void Some_intentionally_failing_test()
+		{
+			Assert.Fail("FAIL 100");
+		}
+	}
+}
